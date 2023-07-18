e6e8eed7bf57b19945be2a0ebf464e6a5579d8f9
Thomas Lu <tsl628@outlook.com>
2023-07-17 23:46:05 -0700

    debug telemetry

--- train.py
+++ train.py
@@ -319,10 +319,9 @@ if __name__ == "__main__":
                 for i in range(3):
                     audio = G_z[i,0,:]
                     writer.add_audio(f'Audio/sample{i}', audio, step)
-                
-                articul_np = G_z.cpu().detach().numpy()
+                            
                 for i in range(num_ch):
-                    articul = articul_np[0,i,:]
+                    articul = G_z[0,i,:].cpu().detach().numpy()
                     fig, ax = plt.subplots()
                     ax.plot(articul, range(len(articul)))
                     writer.add_figure(f"Articul/articul{i}", fig, step)
