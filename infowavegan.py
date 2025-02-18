aec8167a80d9e950f547a8a50b4276fc6669a4e0
Thomas Lu <tsl628@outlook.com>
2025-02-17 21:30:42 -0800

    update dims

--- infowavegan.py
+++ infowavegan.py
@@ -91,7 +91,7 @@ class WaveGANGenerator(torch.nn.Module):
         self.dim_mul = dim_mul
 
         # [100] -> [16, 1024]
-        self.z_project = torch.nn.Linear(latent_dim, 4 * 4 * dim * dim_mul)
+        self.z_project = torch.nn.Linear(latent_dim, 4 * dim * dim_mul)
         self.z_batchnorm = torch.nn.BatchNorm1d(dim*dim_mul) if use_batchnorm else torch.nn.Identity()
         self.avg_pool = torch.nn.AvgPool1d(kernel_size=11, stride = 1, padding = 5, count_include_pad=False)
         dim_mul //= 2
@@ -168,20 +168,15 @@ class WaveGANGenerator(torch.nn.Module):
     def forward(self, z):
         # Project and reshape
         output = self.z_project(z)
-        output = self.z_batchnorm(output.view(-1, self.dim * self.dim_mul, 16))
+        output = self.z_batchnorm(output.view(-1, self.dim * self.dim_mul, 4))
         output = F.leaky_relu(output, self.lrelu_alpha)
 
         # Conv layers
         output = self.upconv0(output)
-        print(output.shape)
         output = self.upconv1(output)
-        print(output.shape)
         output = self.upconv2(output)
-        print(output.shape)
         output = self.upconv3(output)
-        print(output.shape)
         output = self.upconv4(output)
-        print(output.shape)
         # output = self.downconv(output)
         # output = self.avg_pool(output)
         #activation: empirically ema channels in (-4, 4), loudness in (0 ,2), pitch in (80, 255)
